1280cfbe2aa12cbff2fbba7af7051690c732e7a3
Matt Long <matthewhl@gmail.com>
2014-10-05 18:15:12 -0400

    add card text functionality, card shuffling and drawing hands. Some game logic.

--- app/assets/javascripts/deck.js
+++ app/assets/javascripts/deck.js
@@ -7,9 +7,52 @@ $(window).ready(function(){
     name: $("#deck h1").text()
   }
 
+  var manaSymbol = function(mana, line) {
+    function convert(sym) {
+      var symLink;
+      if (sym === "T" || sym === "Q") {
+        symLink = "'http://mtgimage.com/symbol/other/"
+      } else {
+        symLink = "'http://mtgimage.com/symbol/mana/"
+      }
+      return "<span class='mana'><img src=" + symLink + sym + "/64.gif' height='15' width='15'/></span>"
+    }
+
+    var symbols = _.map(mana, function(m){
+      return convert(m[1])
+    }).join("")
+
+    return line.replace(mana.join(""), symbols)
+  }
+
   var Card = Backbone.Model.extend({
     initialize: function(card) {
       this.cid = card.multiverseid
+      this.formatText(card.text, card.name)
+    },
+
+    img: function() {
+      var attrs = this.attributes
+      var tag = "<li><img src='http://mtgimage.com/multiverseid/" + this.cid + ".jpg' "
+      return tag + "multiverseid='" + this.cid + "' height='180' width='120' /></li>"
+    },
+
+    formatText: function(text, name) {
+      if (text) {
+
+        var textLines = text.split("\n")
+        var result = "";
+        var result = _.map(textLines, function(line){
+          var mana = line.match(/\{(\w)\}/g)
+          if (mana !== null) {
+            line = manaSymbol(mana, line);
+          }
+
+          return line
+        }).join("\n")
+
+        this.set({text: result})
+      }
     }
   })
 
@@ -23,9 +66,6 @@ $(window).ready(function(){
         card = new Card(card);
         deck.cards[card.cid] = card
       })
-      // deck.cards = _.map(data.cards, function(card){
-      //   return new Card(card);
-      // })
     }
   })
 
@@ -101,17 +141,42 @@ $(window).ready(function(){
       cards: cards
     })
 
-    var Player = function(deck, name) {
+    var displayHand = function(player) {
+      var hand = _.map(player.hand, function(card){
+        return card.img()
+      }).join("\n")
+
+      player.el.children("div.hand").html(hand)
+
+      $("li img")
+        .mouseenter(function(){
+          var that = $(this)
+          var card = _.find(player.hand, function(c){
+            { return c.cid === parseInt(that.attr("multiverseid")) }
+          })
+          $("div.img-zoom").html("<img src='" + that.attr("src") + "' height='300' width='260'>")
+          if (card.has("text")) {
+            $("div.card-text").html(card.get("text"))
+          }
+        })
+        .mouseleave(function(){
+          $("div.img-zoom").html();
+        })
+    }
+
+    var Player = function(deck, name, n) {
       this.name = name
       this.deck = deck
       this.hand = []
       this.life = 20
       this.cardsInPlay = []
+      this.el = $("#player-" + n)
     }
 
     Player.prototype = {
       draw: function(n) {
         this.hand = this.hand.concat(this.deck.draw(n))
+        displayHand(this)
       },
 
       shuffle: function() {
@@ -124,6 +189,12 @@ $(window).ready(function(){
 
       manaPool: function() {
 
+      },
+
+      turn: function() {
+        this.el.children("div.hand img").on("click", function(){
+
+        })
       }
     }
 
@@ -134,19 +205,32 @@ $(window).ready(function(){
     }
 
     Game.prototype = {
-      placeLibrary: function() {
+      placeLibraries: function() {
         this.playerOne.shuffle()
-        this.playerTwo.shuffle()
+        // this.playerTwo.shuffle()
 
-        
+        function img(player, src) {
+          return $("<img>",{
+            src: src || "http://mtgimage.com/card/cardback.jpg",
+            height: 180,
+            width: 120,
+            name: player.el.attr('id')
+          })
+        }
+
+        $("#player-one div.deck").append(img(playerOne))
+        $("#player-two div.deck").append(img(playerTwo))
+
+        this.playerOne.draw(7)
+        // this.playerTwo.draw(7)
       }
     }
-    var playerOne = new Player(deck, "Matt")
-    var playerTwo = new Player(otherDeck, "Wismer")
+
+    var playerOne = new Player(deck, "Matt", "one")
+    var playerTwo = new Player(otherDeck, "Wismer", "two")
     var game = new Game(playerOne, playerTwo)
 
-    game.showHands();
-
-    debugger
+    game.placeLibraries();
+    $("div.deck").on("click", function(){})
   })
 })
